c56c764a902149d10c079ae1842d660e5d08f052
Tom Sears <tsears@gmail.com>
2016-09-20 17:50:35 -0700

    Initial cut: top users on home page

---
+++ js/apps/global/TopUsers/topUsers.js
@@ -0,0 +1,37 @@
+var topUsersApp = angular.module('topUsersApp', ['toaster']);
+
+topUsersController = topUsersApp.controller('topUsersController', [
+	'$http',
+	'$window', 
+	'$scope', 
+	'toaster',
+	function($http, $window, $scope, toaster) {
+
+		var getDummyUser = function() {
+			return {
+				user: {
+					avatarMedium: '/assets/images/ctlogo64.png',
+				},
+				name: [
+						'User'
+				],
+				count: 0
+			};
+		};
+
+		$scope.topUsers = [];
+		for (var i = 0; i < 5; i++) { $scope.topUsers.push(getDummyUser());}
+
+		$http.get('/_api/users/top?limit=5').then(function(data) {
+			$scope.topUsers = data.data;
+
+			$scope.goToUser = function(user) {
+				$window.location.href = '/User/' + user._id;
+			};
+
+		}, function(err) {
+			// replace with toaster pop
+			alert(err);
+		});
+	}
+]);
